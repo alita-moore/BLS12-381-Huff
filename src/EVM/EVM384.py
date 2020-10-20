e85d5463101f73a576b02aa75122377b14d0486d
alita-moore <alita.moore805@gmail.com>
2020-10-20 01:47:54 -0500

    split the interface from the plugin

--- src/EVM/EVM384.py
+++ src/EVM/EVM384.py
@@ -1,55 +1,62 @@
-from eth._utils.datatypes import Configurable
-from eth.abc import ABC
-from eth.vm.computation import BaseComputation
-from eth.vm.opcode import as_opcode
-from eth_utils import to_canonical_address
-from eth.vm.message import Message
-from eth.vm import opcode_values
-from eth import constants
-from eth.chains.mainnet import MainnetChain
-from eth.db.atomic import AtomicDB
-from eth_utils import to_wei, encode_hex
-MOCK_ADDRESS = constants.ZERO_ADDRESS
-DEFAULT_INITIAL_BALANCE = to_wei(10000, 'ether')
-GENESIS_PARAMS = {
-    'parent_hash': constants.GENESIS_PARENT_HASH,
-    'uncles_hash': constants.EMPTY_UNCLE_HASH,
-    'coinbase': constants.ZERO_ADDRESS,
-    'transaction_root': constants.BLANK_ROOT_HASH,
-    'receipt_root': constants.BLANK_ROOT_HASH,
-    'difficulty': constants.GENESIS_DIFFICULTY,
-    'block_number': constants.GENESIS_BLOCK_NUMBER,
-    'gas_limit': constants.GENESIS_GAS_LIMIT,
-    'extra_data': constants.GENESIS_EXTRA_DATA,
-    'nonce': constants.GENESIS_NONCE
-}
-GENESIS_STATE = {
-    MOCK_ADDRESS: {
-        "balance": DEFAULT_INITIAL_BALANCE,
-        "nonce": 0,
-        "code": b'',
-        "storage": {}
-    }
-}
-chain = MainnetChain.from_genesis(AtomicDB(), GENESIS_PARAMS, GENESIS_STATE)
-def add(computation):
-  return 1
-add_op = as_opcode(add, "ADD", 1)
-def gen_context(chain):
-  CANONICAL_ADDRESS_A = to_canonical_address("0x0f572e5295c57f15886f9b263e2f6d2d6c7b5ec6")
-  CANONICAL_ADDRESS_B = to_canonical_address("0xcd1722f3947def4cf144679da39c4c32bdc35681")
-  state = chain.get_vm().state
-  message = Message(
-    to=CANONICAL_ADDRESS_A,
-    sender=CANONICAL_ADDRESS_B,
-    create_address=None,
-    value=0,
-    data=b'',
-    code=b'',
-    gas=1000000,
-  )
-  tx_context = chain.get_vm()._state_class.transaction_context_class(gas_price=1, origin=CANONICAL_ADDRESS_B)
-  return state, message, tx_context
+from evm_interface import merge, assemble, setup_computation
+from typing import Type
+import copy
 
-state, message, tx = gen_context(chain)
-test = chain.get_vm()._state_class.computation_class(state, message, tx)
+from eth import constants
+from eth.vm.computation import BaseComputation
+from eth.vm.forks.istanbul import IstanbulVM
+from eth.vm.forks.istanbul.computation import IstanbulComputation
+from eth.vm.forks.istanbul.opcodes import ISTANBUL_OPCODES
+from eth.vm.forks.istanbul.state import IstanbulState
+from eth.vm.opcode import as_opcode
+from eth.vm.state import BaseState
+from eth.vm import opcode_values
+
+SUBTRACT_OPCODE_VALUE = 0xa5
+SUBTRACT_MNEMONIC = 'SUBTRACT'
+
+def sub(computation: BaseComputation) -> None:
+    left, right = computation.stack_pop_ints(2)
+
+    result = (2*left - right) & constants.UINT_256_MAX
+
+    computation.stack_push_int(result)
+
+UPDATED_OPCODES = {
+    SUBTRACT_OPCODE_VALUE: as_opcode(
+        logic_fn=sub,
+        mnemonic=SUBTRACT_MNEMONIC,
+        gas_cost=constants.GAS_LOW,
+    )
+}
+
+CUSTOM_OPCODES = merge(
+    copy.deepcopy(ISTANBUL_OPCODES),
+    UPDATED_OPCODES,
+)
+
+class CustomComputation(IstanbulComputation):
+    opcodes = CUSTOM_OPCODES
+
+class CustomState(IstanbulState):
+    computation_class = CustomComputation
+
+class CustomVm(IstanbulVM):
+    _state_class: Type[BaseState] = CustomState
+
+code = assemble(
+    opcode_values.PUSH1,
+    0x1,
+    opcode_values.PUSH1,
+    0x1,
+    SUBTRACT_OPCODE_VALUE
+)
+
+computation = setup_computation(CustomVm, code)
+comp = computation.apply_message(
+        computation.state,
+        computation.msg,
+        computation.transaction_context,
+    )
+result = comp.stack_pop1_any()
+print(result)
--- src/EVM/evm_interface.py
+++ src/EVM/evm_interface.py
@@ -1,10 +1,14 @@
+'''
+This file is an interface between py-evm and this repo; it's a collection 
+of *current* internal methods of the py-evm codebase that when combined 
+deliver desirable uses
+'''
 from typing import NewType, Callable, TypeVar, Union, Type, AnyStr
 import copy
 import re
 import binascii
 from abc import ABCMeta, abstractmethod
 
-# from eth_utils import to_canonical_address
 from eth_hash.auto import keccak
 from eth.vm.message import Message
 from eth.consensus import ConsensusContext
@@ -367,101 +371,3 @@ def setup_computation(vm_class, code):
   )
 
   return computation
-
-# ^ above are internal methods taken from py-evm for the sake of preserving functionality
-##########################################################################################
-
-from eth import constants
-from eth.vm.computation import BaseComputation
-from eth.vm.forks.istanbul import IstanbulVM
-from eth.vm.forks.istanbul.computation import IstanbulComputation
-from eth.vm.forks.istanbul.opcodes import ISTANBUL_OPCODES
-from eth.vm.forks.istanbul.state import IstanbulState
-from eth.vm.opcode import as_opcode
-from eth.vm.state import BaseState
-from eth.vm import opcode_values
-
-SUBTRACT_OPCODE_VALUE = 0xa5
-SUBTRACT_MNEMONIC = 'SUBTRACT'
-
-def sub(computation: BaseComputation) -> None:
-    left, right = computation.stack_pop_ints(2)
-
-    result = (2*left - right) & constants.UINT_256_MAX
-
-    computation.stack_push_int(result)
-
-UPDATED_OPCODES = {
-    SUBTRACT_OPCODE_VALUE: as_opcode(
-        logic_fn=sub,
-        mnemonic=SUBTRACT_MNEMONIC,
-        gas_cost=constants.GAS_LOW,
-    )
-}
-
-CUSTOM_OPCODES = merge(
-    copy.deepcopy(ISTANBUL_OPCODES),
-    UPDATED_OPCODES,
-)
-
-class CustomComputation(IstanbulComputation):
-    opcodes = CUSTOM_OPCODES
-
-class CustomState(IstanbulState):
-    computation_class = CustomComputation
-
-class CustomVm(IstanbulVM):
-    _state_class: Type[BaseState] = CustomState
-
-code = assemble(
-    opcode_values.PUSH1,
-    0x1,
-    opcode_values.PUSH1,
-    0x1,
-    SUBTRACT_OPCODE_VALUE
-)
-
-computation = setup_computation(CustomVm, code)
-comp = computation.apply_message(
-        computation.state,
-        computation.msg,
-        computation.transaction_context,
-    )
-result = comp.stack_pop1_any()
-print(result)
-
-
-# def add(computation):
-#   computation.stack_pop1_int()
-#   computation.stack_push_int(69)
-
-# add_op = as_opcode(add, "ADD", 1)
-
-# # def gen_context(chain):
-  
-# #   state = chain.get_vm().state
-# #   message = Message(
-# #     to=CANONICAL_ADDRESS_A,
-# #     sender=CANONICAL_ADDRESS_B,
-# #     create_address=None,
-# #     value=0,
-# #     data=b'',
-# #     code=b'',
-# #     gas=1000000,
-# #   )
-# #   tx_context = chain.get_vm()._state_class.transaction_context_class(gas_price=1, origin=CANONICAL_ADDRESS_B)
-# #   return state, message, tx_context
-
-# chain = MainnetChain.from_genesis(AtomicDB(), GENESIS_PARAMS, GENESIS_STATE)
-
-# state, message, tx = gen_context(chain)
-# computation = chain.get_vm()._state_class.computation_class(state, message, tx)
-# ####################
-# computation.stack_push_int(1)
-# computation.stack_push_int(1)
-# computation.opcodes[opcode_values.ADD] = add_op
-# computation.opcodes[opcode_values.ADD](computation)
-# result = computation.stack_pop1_int()
-# print(result)
-# # result = 2
-
